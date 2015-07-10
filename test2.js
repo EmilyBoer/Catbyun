c422b64cf9e1b64e8921fbbb4e70c05ad5ab5093
EmilyBoer <Emily.boerner@gmail.com>
2015-07-09 23:53:03 -0700

    Move photos into photo folder

--- test2.js
+++ test2.js
@@ -1,3 +1,5 @@
+// A function that uses the map method to square each number of an array.
+
 
 var numbers = [1,2,3,4,5];
 
@@ -7,8 +9,50 @@ var results = numbers.map(function(eachNumber){
 
 console.log(results);
 
+// function declaration
+forestFright function() {
 
-var greeting = function ();
-alert
+}
 
 
+//Anonymous function (don't need diffOfSquares)
+
+var diff = function diffOfSquares (a,b ) {
+  return a * a - b * b;
+};
+
+diff(4,5);
+// returns answer
+
+console.log(diff);
+// prints entire function
+
+
+//Function Expression have flexibility to select functionality
+var greeting;
+
+// some code that determines if a customer is true or false.
+if (newCustomer){
+  greeting = function(){
+  alert("thanks for visiting the badlands!\n Hope your stay is better than most");
+};
+  else
+}
+
+
+
+
+var customer = true
+
+var greeting = function(){
+  if customer == true;
+  alert("thanks for visiting the badlands!\n Hope your stay is better than most");
+ else customer
+  alert("thanks for visiting the badlands!\n Hope your stay is better than most");
+};
+
+closeTerminal(greeting);
+
+function closeTerminal(message){
+  message();
+}
---
+++ ticket.js
@@ -0,0 +1,17 @@
+var showingTime = [["Die hard", 445] ["Castaway", 700] ["Minions", 1200]]
+
+var notShowing = ["American Hustle", "Speed", "Amy", "End of the Tour"]
+
+movieChoice = "Speed"
+
+var ticket = buildTicket(showingTime, notShowing, movieChoice )
+
+playTime.map(function(movie) {
+  if (movie == playTime][0]); {
+return ("your movie is playing at" + playtime[1])
+  }
+  else if  (movie == notShowing[0]); {
+return ("your movie is not playing tonight")
+  }
+
+};
